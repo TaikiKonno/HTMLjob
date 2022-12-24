2b02125e2a5077966b64f26c9ae2a4baeb27a077
TaikiKonno <117234809+TaikiKonno@users.noreply.github.com>
2022-12-24 13:45:35 +0900

    課題提出テスト
    3章演算子課題

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<?php
+//連結演算子
+$hello="hello";
+$world="world";
+echo $hello.$world;
+echo '<br>';
+//四則演算子
+echo 1+1;
+ echo '<br>';
+ echo 2-1;
+ echo '<br>';
+ echo 1*1;
+ echo '<br>';
+ echo 1/1;
+ echo '<br>';
+ echo 7 % 2;
+ echo '<br>';
+//単項演算子
+$s =5;
+echo  $s++;
+echo '<br>';
+echo $s--;
+
+
+?>
